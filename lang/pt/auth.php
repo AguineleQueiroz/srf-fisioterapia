49bb1ec131067bdcbef3bea6cfb909bb10991446
Aguinele Queiroz <aguinelequeiroz@outlook.com>
2025-03-03 16:18:50 -0300

    configs: configurações iniciais do projeto srf

---
+++ lang/pt/auth.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'failed' => 'Essas credenciais não correspondem aos nossos registros.',
+    'password' => 'A senha fornecida está incorreta.',
+    'throttle' => 'Muitas tentativas de login. Por favor, tente novamente em :seconds segundos.',
+
+    'Login' => 'Entrar',
+];
---
+++ resources/js/layouts/AuthLayout.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import AuthLayout from '@/layouts/auth/AuthSplitLayout.vue';
+import AppLogoIcon from '@/components/AppLogoIcon.vue';
+import { Link } from '@inertiajs/vue3';
+
+defineProps<{
+    title?: string;
+    description?: string;
+}>();
+</script>
+
+<template>
+    <AuthLayout :title="title" :description="description">
+        <slot />
+    </AuthLayout>
+</template>
